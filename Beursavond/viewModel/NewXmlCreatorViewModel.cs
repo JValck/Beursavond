85a47779799d75b66954d6dff07192b1af7c1188
JValck <jasper_de_valck@hotmail.com>
2015-04-27 17:19:54 +0200

    Status bar add with basic info

--- Beursavond/viewModel/NewXmlCreatorViewModel.cs
+++ Beursavond/viewModel/NewXmlCreatorViewModel.cs
@@ -1,20 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
 using System.Windows.Forms;
 
 namespace Beursavond.viewModel {
-    class NewXmlCreatorViewModel {
+    class NewXmlCreatorViewModel : INotifyPropertyChanged{
         private view.NewXmlCreator window;
         private string fileDirectory;
         private string fileName;
+        private string _fileNameDescription;
+        public string FileNameDescription {
+            get {
+                return _fileNameDescription;
+            }
+            set {
+                _fileNameDescription = value;
+                NotifyPropertyChanged();
+            }
+        }
 
         public NewXmlCreatorViewModel(view.NewXmlCreator window) {
             this.window = window;
             askUserForDirectory();
+            generateDefaultFileName();
+        }
+
+        private void generateDefaultFileName() {
+            fileName = "beursavond_" + DateTime.Now.ToString("ddd_d_MMM_yyyy") + ".xml";
+            FileNameDescription = "Bestand: " + fileName;
+
         }
 
         private void askUserForDirectory() {
@@ -25,5 +43,16 @@ namespace Beursavond.viewModel {
 
         }
 
+        #region propertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(
+           [CallerMemberName] String propertyName = "") {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion propertyChanged
     }
 }
